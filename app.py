2764eb56392c89b0e6f23b32020d932fabb97de9
SK SOH <sohsaykiong@gmail.com>
2024-04-17 14:20:40 +0800

    first submission for todo-app flask

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-todo = []
+todo = ["Eat", "Sleep"]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -11,6 +11,13 @@ def index():
     Add a conditional check for the request method.
     If the method is "GET", use render_template to render "index.html" and pass the todo list as a variable.
   """
+  if request.method == "GET":
+    return render_template("index.html", todo = todo)
+  elif request.method == "POST":
+    new_task = request.form.get("task")
+    todo.append(new_task)
+    return render_template("index.html", todo = todo)
+
   # TODO
 
   """
@@ -33,5 +40,8 @@ def remove():
     Remove the specified task from the todo list using todo.remove(task).
     Redirect the user back to the home page.
   """
+  task_del = request.form.get("task")
+  todo.remove(task_del)
+
   # TODO
   return redirect("/")
